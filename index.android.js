1c88c2d502578b86b2cec4112ee811cf0009aa98
Nicole Grieble <ngrieble@expedia.com>
2016-03-22 09:55:32 -0700

    Add dropdown

--- index.android.js
+++ index.android.js
@@ -8,8 +8,17 @@ import React, {
   Component,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
+
+const DropDown = require('react-native-dropdown');
+const {
+  Select,
+  Option,
+  OptionList,
+  updatePosition
+} = DropDown;
 
 var Calendar = require('react-native-calendar-android'),
   moment = require('moment');
@@ -30,11 +39,35 @@ const styles = StyleSheet.create({
 
 var today = moment().format("YYYY/MM/DD");
 
+var RoomPicker = React.createClass({
+  _getOptionList: function() {
+    return this.refs['OPTIONLIST'];
+  },
+
+  render: function() {
+    return (
+        <View>
+          <Select
+            width={250}
+            ref="SELECT1"
+            optionListRef={this._getOptionList.bind(this)}
+            defaultValue="Select a Room">
+              <Option>Standard Room</Option>
+              <Option>Double Room</Option>
+              <Option>Suite</Option>
+            </Select>
+            <OptionList ref="OPTIONLIST"/>
+        </View>
+    )
+  }
+});
+
 class ManageRoomsAndRates extends Component {
   render() {
     return (
       <View style={styles.container}>
       <Text style={styles.header}>Manage Rates And Availability</Text>
+      <RoomPicker />
       <Calendar
         width={300}
         topbarVisible={true}
@@ -46,6 +79,11 @@ class ManageRoomsAndRates extends Component {
         onDateChange={(data) => {
           console.log(data);
         }} />
+        <Text>Availability:</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 150}}
+          value={"80"}
+          />
       </View>
     );
   }
